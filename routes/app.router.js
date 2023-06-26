426a9af8e63cfd355674d445620831365b8ce9d1
keshav sharma <ks98eng@gmail.com>
2023-06-26 12:52:21 +0530

    login from get to post

--- routes/app.router.js
+++ routes/app.router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const appController = require('../controllers/app.controller')
 
 router.post("/sign-up", appController.signUp)
-router.get("/login",appController.login)
+router.post("/login",appController.login)
 router.get("/",appController.Hello)
 
 module.exports = router
